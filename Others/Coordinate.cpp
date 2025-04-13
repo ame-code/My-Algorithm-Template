219627e9da191f0c674841641505ae5ea04d6aaf
aleaf <1457882960@qq.com>
2025-04-13 23:47:52 +0800

    修复Coordinate诸多bug

--- Others/Coordinate.cpp
+++ Others/Coordinate.cpp
@@ -1,178 +1,410 @@
 #include<bits/stdc++.h>
-template<std::integral T = int>
+
+template<std::integral T>
 class Coordinate {
 public:
-    using Integer = T;
+    struct Coor
+    {
+        // 坐标
+        T x = 0;
+        T y = 0;
 
-    // 坐标
-    Integer x = 0;
-    Integer y = 0;
-    // 网格边界
-    static Integer xAxisLowBound, xAxisUpBound, yAxisLowBound, yAxisUpBound;
-    // 静态方向向量(4方向)
-    inline static constexpr std::array<Coordinate<Integer>, 4> directions4 = {
-        Coordinate{0, 1}, {1, 0}, {0, -1}, {-1, 0}
-    };
-    inline static constexpr std::array<Coordinate<Integer>, 8> directions8 = {
-        Coordinate{-1, -1}, {-1, 0}, {-1, 1}, {0, -1},
-        {0, 1}, {1, -1}, {1, 0}, {1, 1}
-    };
+        // 构造函数
+        constexpr Coor() noexcept : x(0), y(0) {}
+        constexpr Coor(T a, T b) noexcept : x(a), y(b) {};
+        constexpr Coor(const std::pair<T, T> p) noexcept : x(p.first), y(p.second) {}
+        constexpr Coor(const Coor& coor) noexcept : x(coor.x), y(coor.y) {}
+        constexpr Coor(Coor&& coor) noexcept : x(coor.x), y(coor.y) {}
 
-    // 构造函数
-    Coordinate() : x(0), y(0) {};
-    Coordinate(Integer x, Integer y) : x(x), y(y) {};
-    Coordinate(const Coordinate &coor) : x(coor.x), y(coor.y) {};
-    Coordinate(Coordinate&& other) noexcept : x(other.x), y(other.y) {}
-
-    // 重载运算符部分
-    // 坐标相加
-    Coordinate<Integer> operator+(const Coordinate& coor) const {
-        return {x + coor.x, y + coor.y};
-    }
-    // 坐标加等于另一个坐标
-    Coordinate<Integer>& operator+=(const Coordinate& coor) {
-        x += coor.x, y += coor.y;
-        return *this;
-    }
-    // 坐标相减
-    Coordinate<Integer> operator-(const Coordinate& coor) const {
-        return {x - coor.x, y - coor.y};
-    }
-    // 坐标减等于另一个坐标
-    Coordinate<Integer>& operator-=(const Coordinate& coor) {
-        x -= coor.x, y -= coor.y;
-        return *this;
-    }
-    // 坐标乘一个整数
-    Coordinate<Integer> operator*(const Integer scalar) const {
-        return {x * scalar, y * scalar};
-    }
-    // 坐标乘等于整数
-    Coordinate<Integer>& operator*=(const Integer& scalar) {
-        this->x *= scalar, this->y *= scalar;
-        return *this;
-    }
-    // 坐标除以一个整数
-    Coordinate<Integer> operator/(const Integer num) const {
-        return {x / num, y / num};
-    }
-    // 坐标除等于一个整数
-    Coordinate<Integer>& operator/=(const Integer num) {
-        this->x /= num, this->y /= num;
-        return *this;
-    }
-    // 坐标取余于一个整数
-    Coordinate<Integer> operator%(const Integer num) const {
-        return {x % num, t % num};
-    }
-    // 坐标取余等于一个整数
-    Coordinate<Integer> operator%=(const Integer num) {
-        this->x %= num, this->y %= num;
-        return *this;
-    }
-    // 判断坐标是否相等
-    bool operator==(const Coordinate& coor) const {
-        return x == coor.x && y == coor.y;
-    }
-    // 小于运算符(与哈希函数一起,用于兼容STL容器)(字典序大小)
-    bool operator<(const Coordinate& coor) const {
-        return x == coor.x ? y < coor.y : x < coor.x;
-    }
-    // 大于运算符(字典序大小)
-    bool operator>(const Coordinate& coor) const {
-        return x == coor.x ? y > coor.y : x > coor.x;
-    }
-    // 小于等于运算符
-    bool operator<=(const Coordinate& coor) const {
-        return x == coor.x ? y <= coor.y : x <= coor.x;
-    }
-    // 大于等于运算符
-    bool operator>=(const Coordinate& coor) const {
-        return x == coor.x ? y >= coor.y : x >= coor.x;
-    }
-    // 静态成员函数部分
-    // 两点间的距离
-    static double distance(const Coordinate& coor1, const Coordinate& coor2) {
-        if constexpr (std::is_same<Integer, long long>::value) {
-            return std::sqrtl((coor1.x - coor2.x) * (coor1.x - coor2.x) + (coor1.y - coor2.y) * (coor1.y - coor2.y));
-        } else {
-            return std::sqrt((coor1.x - coor2.x) * (coor1.x - coor2.x) + (coor1.y - coor2.y) * (coor1.y - coor2.y));
+        // 重载运算符
+        // +
+        Coor operator+(const Coor& coor) {
+            return Coor(x + coor.x, y + coor.y);
         }
+        // +=
+        Coor& operator+=(const Coor& coor) {
+            this->x += coor.x;
+            this->y += coor.y;
+            return *this;
+        }
+        // -
+        Coor operator-(const Coor& coor) {
+            return Coor(x - coor.x, y - coor.y);
+        }
+        // -=
+        Coor& operator-=(const Coor& coor) {
+            this->x -= coor.x;
+            this->y -= coor.y;
+            return *this;
+        }
+        // *
+        Coor operator*(T num) {
+            return Coor(x * num, y * num);
+        }
+        // *= 
+        Coor& operator*=(T num) {
+            this->x *= num;
+            this->y *= num;
+            return *this;
+        }
+        // /
+        Coor operator/(T num) {
+            return Coor(x / num, y / num);
+        }
+        // /=
+        Coor& operator/=(T num) {
+            this->x /= num;
+            this->y /= num;
+            return *this;
+        }
+        // %
+        Coor operator%(T num) {
+            return Coor(x % num, y % num);
+        }
+        // %=
+        Coor& operator%=(T num) {
+            this->x %= num;
+            this->y %= num;
+            return *this;
+        }
+        Coor operator&(T num) {
+            return Coor(x & num, y & num);
+        }
+        Coor& operator&=(T num) {
+            this->x &= num;
+            this->y &= num;
+            return *this;
+        }
+        Coor operator|(T num) {
+            return Coor(x | num, y | num);
+        }
+        Coor& operator|=(T num) {
+            this->x |= num;
+            this->y |= num;
+            return *this;
+        }
+        Coor operator^(T num) {
+            return Coor(x ^ num, y ^ num);
+        }
+        Coor& operator^=(T num) {
+            this->x ^= num;
+            this->y ^= num;
+            return *this;
+        }
+        Coor operator>>(T num) {
+            return Coor(x >> num, y >> num);
+        }
+        Coor& operator>>=(T num) {
+            this->x >>= num;
+            this->y >>= num;
+            return *this;
+        }
+        Coor operator<<(T num) {
+            return Coor(x << num, y << num);
+        }
+        Coor& operator<<=(T num) {
+            this->x <<= num;
+            this->y <<= num;
+            return *this;
+        }
+        Coor& operator=(const Coor& coor) {
+            this->x = coor.x;
+            this->y = coor.y;
+            return *this;
+        }
+        Coor& operator=(const std::pair<T, T>& p) {
+            this->x = p.first;
+            this->y = p.second;
+            return *this;
+        }
+        Coor& operator=(const T num[2]) {
+            this->x = num[0];
+            this->y = num[1];
+            return *this;
+        }
+
+        // 比较运算符
+        bool operator<(const Coor& coor) const {
+            return x == coor.x ? y < coor.y : y < coor.y;
+        }
+        bool operator<=(const Coor& coor) const{
+            return x <= coor.x && y <= coor.y;
+        }
+        bool operator>(const Coor& coor) const {
+            return x == coor.x ? y > coor.y : y > coor.y;
+        }
+        bool operator>=(const Coor& coor) const {
+            return x >= coor.x && y >= coor.y;
+        }
+        bool operator==(const Coor& coor) const {
+            return x == coor.x && y == coor.y;
+        }
+        bool operator!=(const Coor& coor) const {
+            return x != coor.x && y != coor.y;
+        }
+    };
+
+    Coor coor;
+    static constexpr std::array<Coor, 4> directions4 = {
+        Coor(0, 1), Coor(0, -1), Coor(1, 0), Coor(-1, 0)
+    };
+    static constexpr std::array<Coor, 8> directions8 = {
+        Coor(0, 1), Coor(0, -1), Coor(1, 0), Coor(-1, 0),
+        Coor(1, 1), Coor(1, -1), Coor(1, -1), Coor(-1, -1)
+    };
+    static Coor lowerBound, upperBound;
+
+    constexpr Coordinate(): coor(0,0) {}
+    constexpr Coordinate(int a, int b): coor(a, b) {}
+    constexpr Coordinate(const std::pair<T, T> p): coor(p) {}
+    constexpr Coordinate(const Coor& coor): coor(coor) {}
+    constexpr Coordinate(const Coordinate& other): coor(other.coor) {}
+    constexpr Coordinate(Coordinate&& other): coor(other.coor) {}
+
+    // 重载运算符
+    Coordinate operator+(const Coordinate& other) const {
+        return coor + other;
     }
-    // 设置边界上界
-    static void setBound(const int xAxisUpBound, const int yAxisUpBound) {
-        Coordinate::xAxisUpBound = xAxisUpBound;
-        Coordinate::yAxisUpBound = yAxisUpBound;
+    Coordinate operator-(const Coordinate& other) const {
+        return coor - other;
     }
-    // 设置边界下界
-    static void setBound(const int xAxisLowBound, const int xAxisUpBound, const int yAxisLowBound, const int yAxisUpBound) {
-        Coordinate::xAxisLowBound = xAxisLowBound;
-        Coordinate::xAxisUpBound = xAxisUpBound;
-        Coordinate::yAxisLowBound = yAxisLowBound;
-        Coordinate::yAxisUpBound = yAxisUpBound;
+    Coordinate operator*(T num) const {
+        return coor * num;
+    }
+    Coordinate operator/(T num) const {
+        return coor / num;
+    }
+    Coordinate operator%(T num) const {
+        return coor % num;
+    }
+    Coordinate& operator+=(const Coordinate& other) {
+        coor += other;
+        return *this;
+    }
+    Coordinate& operator-=(const Coordinate& other) {
+        coor += other;
+        return *this;
+    }
+    Coordinate& operator*=(T num) {
+        coor += num;
+        return *this;
+    }
+    Coordinate& operator/=(T num) {
+        coor += num;
+        return *this;
+    }
+    Coordinate operator^(T num) const {
+        return coor ^ num;
+    }
+    Coordinate operator|(T num) const {
+        return coor | num;
+    }
+    Coordinate operator&(T num) const {
+        return coor & num;
+    }
+    Coordinate operator<<(T num) const {
+        return coor << num;
+    }
+    Coordinate operator>>(T num) const {
+        return coor + num;
+    }
+    Coordinate& operator^=(T num) {
+        coor += num;
+        return *this;
+    }
+    Coordinate& operator|=(T num) {
+        coor += num;
+        return *this;
+    }
+    Coordinate& operator&=(T num) {
+        coor += num;
+        return *this;
+    }
+    Coordinate& operator<<=(T num) {
+        coor += num;
+        return *this;
+    }
+    Coordinate& operator>>=(T num) {
+        coor += num;
+        return *this;
+    }
+    Coordinate& operator=(const Coordinate& other) {
+        coor = other.coor;
+        return *this;
+    }
+    Coordinate& operator=(const T num[2]) {
+        coor = num;
+        return *this;
+    }
+    Coordinate& operator=(const std::pair<T, T> p) {
+        coor = p;
+        return *this;
+    }
+    bool operator>(const Coordinate& other) const {
+        return coor > other;
+    }
+    bool operator<(const Coordinate& other) const {
+        return coor >= other;
+    }
+    bool operator>=(const Coordinate& other) const {
+        return coor < other;
+    }
+    bool operator<=(const Coordinate& other) const {
+        return coor <= other;
+    }
+    bool operator==(const Coordinate& other) const {
+        return coor == other;
+    }
+    bool operator!=(const Coordinate& other) const {
+        return coor != other;
+    }
+
+    // 静态函数部分
+    static bool setUpperBound(T rowUpperBound, T columnUpperBound) {
+        if(rowUpperBound < upperBound.x || columnUpperBound < upperBound.y) {
+            return false;
+        } else {
+            upperBound.x = rowUpperBound;
+            upperBound.y = columnUpperBound;
+            return true;
+        }
+        return false;
+    }
+    static bool setUpperBound(const Coor& coor) {
+        if(coor.x < upperBound.x || coor.y < upperBound.y) {
+            return false;
+        } else {
+            upperBound = coor;
+            return true;
+        }
+        return false;
+    }
+    static bool setUpperBound(const Coordinate& other) {
+        if(other.coor.x < upperBound.x || other.coor.y < upperBound.y) {
+            return false;
+        } else {
+            upperBound = other.coor;
+            return true;
+        }
+        return false;
+    }
+    static bool setLowerBound(T rowLowerBound, T columnLowerBound) {
+        if(rowLowerBound > lowerBound.x || columnLowerBound > lowerBound.y) {
+            return false;
+        } else {
+            lowerBound.x = rowLowerBound;
+            lowerBound.y = columnLowerBound;
+            return true;
+        }
+        return false;
+    }
+    static bool setLowerBound(const Coor& coor) {
+        if(coor.x > upperBound.x || coor.y > upperBound.y) {
+            return false;
+        } else {
+            upperBound = coor;
+            return true;
+        }
+        return false;
+    }
+    static bool setLowerBound(const Coordinate& other) {
+        if(other.coor.x > upperBound.x || other.coor.y > upperBound.y) {
+            return false;
+        } else {
+            upperBound = other.coor;
+            return true;
+        }
+        return false;
+    }
+    static bool setBound(T rowLowerBound, T columnLowerBound, T rowUpperBound, T columnUpperBound) {
+        if(setLowerBound(rowLowerBound, columnLowerBound)) {
+            if(setUpperBound(rowUpperBound, columnUpperBound)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        return false;
+    }
+    // 两点间距离
+    // 绝对距离
+    static double distance(const Coordinate& coor1, const Coordinate coor2) {
+        if constexpr (std::is_same<std::remove_cvref_t<T>, long long>::value) {
+            Coor temp = coor1 - coor2;
+            return std::sqrtl(temp.x * temp.x + temp.y * temp.y);
+        } else {
+            Coor temp = coor1 - coor2;
+            return std::sqrt(temp.x * temp.x + temp.y * temp.y);
+        }
+        return false;
     }
     // 曼哈顿距离
-    static Integer manhattan(const Coordinate& a, const Coordinate& b) {
-        return std::abs(a.x - b.x) + std::abs(a.y - b.y);
+    static T manhattan(const Coordinate& coor1, const Coordinate& coor2) {
+        Coor temp = coor1.coor - coor2.coor;
+        return std::abs(temp.x) + std::abs(temp.y);
+    }
+    static T manhattan(const Coor& coor1, const Coor& coor2) {
+        Coor temp = coor1 - coor2;
+        return std::abs(temp.x) + std::abs(temp.y);
+    }
+    static T manhattan(T coor1x, T coor1y, T coor2x, T coor2y) {
+        Coor temp = Coor(coor1x - coor2x, coor1y - coor2y);
+        return std::abs(temp.x) + std::abs(temp.y);
     }
     // 切比雪夫距离
-    static Integer chebyshev(const Coordinate& a, const Coordinate& b) {
-        return std::max(std::abs(a.x - b.x), std::abs(a.y - b.y));
+    static T chebyshev(const Coordinate& coor1, const Coordinate& coor2) {
+        Coor temp = coor1.coor - coor2.coor;
+        return std::max(std::max(temp.x), std::max(temp.y));
+    }
+    static T chebyshev(const Coor& coor1, const Coor& coor2) {
+        Coor temp = coor1 - coor2;
+        return std::max(std::max(temp.x), std::max(temp.y));
+    }
+    static T chebyshev(T coor1x, T coor1y, T coor2x, T coor2y) {
+        Coor temp = Coor(coor1x - coor2x, coor1y - coor2y);
+        return std::max(std::max(temp.x), std::max(temp.y));
     }
 
-    // 部分功能的实现
-    void setPos(int x, int y) {
-        this->x = x;
-        this->y = y;
+    // 针对当前点的功能
+    // 设置坐标
+    void setCoor(int x, int y) {
+        coor.x = x;
+        coor.y = y;
     }
-    // 判断是否越界
-    bool notOutEdge() const noexcept {
-        return x >= xAxisLowBound && x <= xAxisUpBound && y >= yAxisLowBound && y <= yAxisUpBound;
+    // 判断越界
+    bool notOutBound() const {
+        return coor >= lowerBound && coor <= upperBound;
     }
-    // 获取相邻坐标并过滤越界
-    std::vector<Coordinate> neighbors(bool allowDiagonal = false) const noexcept {
-        const auto& dirs = allowDiagonal ? directions8 : directions4;
-        std::vector<Coordinate<Integer>> ret;
+    std::vector<Coordinate> neighbors(bool allowDiagonal = false) const {
+        std::vector<Coordinate> ret;
         ret.reserve(allowDiagonal ? 8 : 4);
-        for (auto &it : dirs) {
-            Coordinate temp = *this + it;
-            if (temp.notOutEdge())
-                ret.push_back(temp);
+
+        if(allowDiagonal) {
+            for(auto& offset : directions8) {
+                Coordinate temp = coor + offset;
+                if(temp.notOutBound()) {
+                    ret.push_back(temp);
+                }
+            }
+        } else {
+            for(auto& offset : directions4) {
+                Coordinate temp = coor + offset;
+                if(temp.notOutBound()) {
+                    ret.push_back(temp);
+                }
+            }
         }
+
         return ret;
     }
-    // 重载输出用于调试
-    friend std::ostream& operator<<(std::ostream &outStream, const Coordinate& coor) {
-        return outStream << '(' << coor.x << ',' << coor.y << ')';
-    }
-    // 哈希函数支持
-    struct Hash
-    {
-        size_t operator()(const Coordinate& coor) const {
-            size_t hash1 = std::hash<Integer>()(coor.x);
-            size_t hash2 = std::hash<Integer>()(coor.y);
-            return hash1 ^ (hash2 + 0x9e3779b9 + (hash1 << 6) + (hash1 >> 2));
-        }
-    };
-    // x1 <= x <= x2 | y1 <= y <= y2
-    bool inRectangle(int x1, int y1, int x2, int y2) const {
-        if (x1 > x2) std::swap(x1, x2);
-        if (y1 > y2) std::swap(y1, y2);
-        return x >= x1 && x <= x2 && y >= y1 && y <= y2;
-    }
-    // coor1.x <= x <= coor2.y | coor1.y <= y <= coor2.y
-    bool inRectangle(Coordinate coor1, Coordinate coor2) const {
-        return inRectangle(coor1.x, coor1.y, coor2.x, coor2.y);
+    friend std::ostream& operator<<(std::ostream& os, const Coordinate& coor) {
+        return os << std::format("({},{})", coor.coor.x, coor.coor.y);
     }
 };
-template <std::integral Integer>
-Integer Coordinate<Integer>::xAxisLowBound = 0;
 
-template <std::integral Integer>
-Integer Coordinate<Integer>::xAxisUpBound = INT_MAX;
-
-template <std::integral Integer>
-Integer Coordinate<Integer>::yAxisLowBound = 0;
-
-template <std::integral Integer>
-Integer Coordinate<Integer>::yAxisUpBound = INT_MAX;
+template<std::integral T>
+Coordinate<T>::Coor Coordinate<T>::lowerBound(0,0);
+template<std::integral T>
+Coordinate<T>::Coor Coordinate<T>::upperBound(INT_MAX, INT_MAX);
