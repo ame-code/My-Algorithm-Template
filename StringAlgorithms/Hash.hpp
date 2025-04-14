cbb7a28ddc77b283a561d00e50e2ab439f08ec48
aleaf <1457882960@qq.com>
2025-04-14 19:21:52 +0800

    modified:   Hash.hpp

--- StringAlgorithms/Hash.hpp
+++ StringAlgorithms/Hash.hpp
@@ -1,60 +1,100 @@
-#include<bits/stdc++.h>
+#pragma once
+#ifndef HASH_HEADER
+#define HASH_HEADER
 
-class Hash {
+#include <array>
+#include <vector>
+#include <string>
+#include <utility>
+#include <ranges>
+
+enum class HashType{array, vector};
+
+template<HashType hashtype>
+class Hash;
+
+template<>
+class Hash<HashType::array> {
 private:
-    using uint64 = unsigned long long;
-    int base = 233;
-    std::vector<uint64> hashValue;
+    constexpr static size_t powArraySize = 10'000;
+    constexpr static size_t baseFisrt = 131;
+    constexpr static size_t baseSecond = 233;
+    constexpr static auto calPowArray = [](size_t baseFirst, size_t baseSecond) constexpr -> std::array<std::pair<size_t, size_t>, powArraySize + 1> {
+        std::array<std::pair<size_t, size_t>, powArraySize + 1> arr{};
+        arr[0] = std::pair<size_t, size_t>(1ULL, 1ULL);
+        for(size_t i : std::views::iota(1ULL, powArraySize + 1)) {
+            arr[i].first = arr[i - 1].first * baseFirst;
+            arr[i].second = arr[i - 1].second * baseSecond;
+        }
+        return arr;
+    };
+    constexpr static std::array<std::pair<size_t, size_t>, powArraySize + 1> powValue = calPowArray(baseFisrt, baseSecond);
+    std::vector<std::pair<size_t, size_t>> hashValue;
+    
 public:
-    Hash():hashValue(0) {}
-    Hash(const std::string& s) {
-        calculateHash(s);
+    constexpr Hash(): hashValue(0) {}
+    constexpr Hash(size_t size): hashValue(size + 1, std::pair<size_t, size_t>(0ULL, 0ULL)) {}
+    Hash(const std::string& str) {
+        calcuteHash(str);
     }
-    Hash(const std::string& s, int base):base(base) {
-        calculateHash(s);
-    }
-    Hash(const Hash& h):hashValue(h.hashValue), base(base) {}
+    constexpr Hash(Hash& hash): hashValue(hash.hashValue) {}
 
-    void calculateHash(const std::string& s) {
-        hashValue.assign(s.length() + 1,0);
-        int l = s.length();
-        for(int i = 0; i < l; i++) {
-            hashValue[i + 1] = hashValue[i] * base + s[i];
+    void calcuteHash(const std::string str) {
+        size_t len = str.length();
+        hashValue.assign(len + 1, std::pair<size_t, size_t>(0ULL, 0ULL));
+        for(size_t i : std::views::iota(0ULL, len)) {
+            hashValue[i + 1].first = hashValue[i].first * baseFisrt + str[i];
+            hashValue[i + 1].second = hashValue[i].second * baseSecond + str[i];
         }
     }
 
-    uint64 getHash(int l, int r) {
-        return hashValue[r] - hashValue[l - 1];
-    }
-
-    void setBase(int base) {
-        this->base = base;
+    std::pair<size_t, size_t> get(size_t startPos = 0, size_t size = ULONG_LONG_MAX) {
+        size = std::min(size, hashValue.size() - 1);
+        size_t l = startPos + 1;
+        size_t r = l + size - 1;
+        return std::make_pair(hashValue[r].first - hashValue[l - 1].first * powValue[r - l + 1].first, hashValue[r].second - hashValue[l - 1].second * powValue[r - l + 1].second);
     }
 };
 
-class HashTwice {
+template<>
+class Hash<HashType::vector> {
 private:
-    using uint64 = unsigned long long;
-    int base1 = 233;
-    int base2 = 137;
-    std::vector<std::pair<uint64, uint64>> hashValue;
+    constexpr static size_t baseFisrt = 131;
+    constexpr static size_t baseSecond = 233;
+    std::vector<std::pair<size_t, size_t>> hashValue;
+    std::vector<std::pair<size_t, size_t>> powValue;
+    
 public:
-    HashTwice():hashValue(0) {}
-    HashTwice(const int& base1, const int& base2):base1(base1), base2(base2){}
-    HashTwice(const std::string s) {
-
+    constexpr Hash(): hashValue(0), powValue(1, std::pair<size_t, size_t>(1ULL, 1ULL)) {}
+    constexpr Hash(size_t size): hashValue(size + 1, std::pair<size_t, size_t>(0ULL, 0ULL)), powValue(1, std::pair<size_t, size_t>(1ULL, 1ULL)) {}
+    Hash(const std::string& str): powValue(1, std::pair<size_t, size_t>(1ULL, 1ULL)){
+        calcuteHash(str);
     }
+    constexpr Hash(Hash& hash): hashValue(hash.hashValue), powValue(hash.powValue) {}
 
-    void calculteateHashTwice(const std::string& s) {
-        hashValue.assign(s.length() + 1, std::pair<uint64, uint64>(0 ,0));
-        int l = s.length();
-        for(int i = 0; i < l; i++) {
-            hashValue[i + 1].first = hashValue[i].first * base1 + s[i];
-            hashValue[i + 1].second = hashValue[i].second * base2 + s[i];
+    void calcuteHash(const std::string str) {
+        size_t len = str.length();
+        hashValue.assign(len + 1, std::pair<size_t, size_t>(0ULL, 0ULL));
+        if(len + 1 > powValue.size()) {
+            size_t n = powValue.size();
+            powValue.resize(len + 1);
+            for(size_t i : std::views::iota(n, len + 1)) {
+                powValue[i].first = powValue[i - 1].first * baseFisrt;
+                powValue[i].second = powValue[i - 1].second * baseSecond;
+            }
+        }
+        for(size_t i : std::views::iota(0ULL, len)) {
+            hashValue[i + 1].first = hashValue[i].first * baseFisrt + str[i];
+            hashValue[i + 1].second = hashValue[i].second * baseSecond + str[i];
         }
     }
 
-    std::pair<uint64,uint64> getHashPair(int l, int r) {
-        return std::pair<uint64, uint64>(hashValue[r].first - hashValue[l - 1].first, hashValue[r].second - hashValue[l - 1].second);
+    std::pair<size_t, size_t> get(size_t startPos = 0, size_t size = ULONG_LONG_MAX) {
+        size = std::min(size, hashValue.size() - 1);
+        size_t l = startPos + 1;
+        size_t r = l + size - 1;
+        return std::make_pair(hashValue[r].first - hashValue[l - 1].first * powValue[r - l + 1].first, hashValue[r].second - hashValue[l - 1].second * powValue[r - l + 1].second);
     }
 };
+
+#endif
