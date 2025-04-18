dcc9142257421429f1a59829c534c49c52dee050
aleaf <1457882960@qq.com>
2025-04-18 19:03:11 +0800

    修改了二分查找板子

--- AlgorithmFundamentals/BinarySearch.hpp
+++ AlgorithmFundamentals/BinarySearch.hpp
@@ -4,32 +4,24 @@
 
 // ========================
 // 二分搜索
-// 最后修改日期：2025-04-15
+// 最后修改日期：2025-04-18
 // ========================
 
 #include<concepts>
 
 namespace aleaf {
-    enum class TrueBinaryInterval{right, left};
 
-    template<TrueBinaryInterval tag, std::integral INT = int, typename FUNC>
-    INT binarySearch(INT l, INT r, INT goal, FUNC check) {
-        INT ret = -1;
+    // 二分查找第一个使check为true的值
+    template<typename FUNC, std::integral INT = int, INT RET = -1>
+    INT binarySearch(INT l, INT r, FUNC check) {
+        INT ret = RET;
         while (l <= r) {
             INT mid = l + ((r - l) >> 1);
-            if (check(mid, goal)) {
+            if (check(mid)) {
                 ret = mid;
-                if constexpr (tag == TrueBinaryInterval::right) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
+                r = mid - 1;
             } else {
-                if constexpr (tag == TrueBinaryInterval::right) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
+                l = mid + 1;
             }
         }
         return ret;
